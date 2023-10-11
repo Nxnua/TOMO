3b4eb7f57f2216d1daeb9d62b5040a9787deca7b
gunjee007 <120143124+gunjee007@users.noreply.github.com>
2023-10-11 18:33:01 +0800

    laravel

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    const heading = "Laravel 9 Vite  with React JS";
+    return <div> 
+         <h1 class="text-3xl font-bold underline text-blue-500">
+    Hello world!
+  </h1>
+
+  <p className=" bg-slate-400">sdljg</p>
+  <p className=" bg-slate-400">sdljg</p>
+    </div>
+}
--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import "./index.css"
-import { LoginSignup } from "./pages/sign-up"
-export default function App() {
-  return (
-    <LoginSignup/>
-  )
-}
