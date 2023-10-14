473b6cc9e2b52d20d2344d003a4d398a29579592
Gunjee007 <Gunjee163@gmail.com>
2023-10-15 00:40:33 +0800

    start heseg

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,21 +1,12 @@
-// export default function Home() {
-//     return <div className="flex">
-        
-//     </div>
-// }
-
 import Banner from "../components/banner";
+import Start from "../components/start";
 export default function Home() {
-    return (
-        <div>
-            <div className="flex relative">
-                <Banner />
-                <div className="z-20 inset-y-0 bg-white w-[500px] rounded-l-2xl drop-shadow-[-26px_-28px_49px_rgba(0,0,0,0.30)]">
-          <div className=" flex justify-end m-4">light mode</div>
-          <h1 className="flex justify-center m-4 text-3xl font-extrabold tracking-tight text-[color:var(--orange)] ">Hi, I'm Tomo!</h1>
-
-        </div>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="flex relative">
+        <Banner />
+        <Start />
+      </div>
+    </div>
+  );
 }
--- resources/js/components/banner.jsx
+++ resources/js/components/banner.jsx
@@ -1,22 +1,10 @@
 export default function Banner() {
     return (
-        // <div className="flex-1 bg-light-banner bg-no-repeat bg-cover h-screen mr-[500px]">
-        //     {/* <img src="/images/background.png" alt="" className=" h-screen" /> */}
-        //     <img src="/images/roadlines.png" className="z-0 absolute bottom-[calc(100vh - 700px)] left-[20px]"/>
-        //     <img
-        //         src="/images/car.png"
-        //         className="h-[20vh] absolute bottom-[20vh] left-[200px]"
-        //     />
-        //     <img src="/images/bushes.png" className="  absolute bottom-[0px] left-[5px]"/>
-        // </div>
-        <div className=" flex-1">
-        <div className="relative z-10">
-          <img src="/images/background.png" alt="" className="h-screen" />
-           <img src="/images/roadlines.png" className="z-0  absolute bottom-[23vh] left-[20px]"/>
-           <img src="/images/car.png" className="h-[20vh]  absolute bottom-[23vh] left-[200px]"/>
-           <img src="/images/bushes.png" className="  absolute bottom-[0px] left-[5px]"/>
-        </div>
-        
+            <div className="relative z-10">
+                <img src="/images/background.png" alt="" className="h-screen" />
+                <img src="/images/roadlines.png" className="z-0  absolute bottom-[23vh] left-[20px]"/>
+                <img src="/images/car.png" className="z-0 h-[20vh] left-[22vw]  absolute bottom-[23vh] "/>
+                <img src="/images/bushes.png" className=" h-[15vh] absolute bottom-[0vh] left-[5vw]"/>
         </div>
     );
 }
