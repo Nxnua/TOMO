c0bad18df64fa41be186d3328b2f0da6e6cb900e
Gunjee007 <Gunjee163@gmail.com>
2023-10-18 21:57:15 +0800

    onscroll

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,25 +1,16 @@
+
+import Landing from "./LandingPage";
 import { useState } from "react";
-import Banner from "../components/banner";
-import SignUp from "../components/signup";
-import Start from "../components/start";
 export default function Home() {
-    const [onStart, setOnStart] = useState(false);
+    const [scroll, setScroll] = useState(false);
+    const handleScroll = (event) => {
+        setScroll(event.currentTarget.scrollTop);
+      };
+  
+
     return (
-        <div>
-            <div className="flex tracking-tight relative bg-[#bcf5f7]">
-                <Banner />
-                <div className="z-20 absolute right-0 inset-y-0 bg-white w-[500px] rounded-l-3xl drop-shadow-[-26px_-28px_49px_rgba(0,0,0,0.40)]">
-                    {onStart ? (
-                        <SignUp />
-                    ) : (
-                        <Start
-                            startHandler={() => {
-                                setOnStart(true);
-                            }}
-                        />
-                    )}
-                </div>
-            </div>
+        <div onScroll={()=> setScroll(true)}>
+            <Landing scroll = {scroll} />
         </div>
     );
 }
