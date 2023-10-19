83b0015fc27020063260f511bf626804a680eb70
kisaragi1234 <beeerhembileg@gmail.com>
2023-10-19 13:48:11 +0800

    Guides

---
+++ resources/js/Pages/Guides.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+// import "./navbar"
+
+
+export const Guides = () => {
+  return (
+    <div>
+        <div className="guideNavbar">
+            <div className="guideNavbarWrapper flex items-center scroll-p-6 justify-between">
+                <div className="item bg-FFFFFF relative flex items-center cursor-pointer">
+                    <span>All guide</span>
+                </div>
+                <div className="item">
+                    <span>All guide</span>
+                </div>
+                <div className="item">
+                    <span>All guide</span>
+                </div>
+                <div className="item">
+                    <span>All guide</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
