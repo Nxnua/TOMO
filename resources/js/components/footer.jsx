f48605274c7f457462520b4cd4c77481188caed8
Gunjee007 <Gunjee163@gmail.com>
2023-10-19 16:09:16 +0800

    sdf

---
+++ resources/js/components/footer.jsx
@@ -0,0 +1,47 @@
+export default function Footer(){
+    const menu = [{ id: 1, title :"Trips", link : ""}, { id: 2,title :"Guides", link : ""},{ id: 3,title :"Agencies", link : ""},{ id: 4,title :"Reviews", link : ""},];
+    const tripPlanner = [{id: 1, title :"Plan a trip", link : ""}, { id: 2, title :"Travel Budget", link : ""},{ id:3, title :"Flights", link : ""}];
+    return(
+        
+        <div className=" bg-black75 flex flex-col ">
+            <div className=" h-72 ">
+                <div className=" h-full grid grid-cols-3">
+                    <div className=" relative flex items-center ">
+                        <img src="/images/tomo-T.png" alt="" className=" absolute  h-52 ml-36   " />
+                        <img src="/images/Tomo-White.png" alt="" className=" h-20 ml-44 " />
+                    </div>
+                    <div className="flex flex-row p-12 justify-between ">
+                        <div>
+                            <p className=" text-2xl text-orange font-extrabold">Menu</p>
+
+                            <div className=" flex flex-col">
+                                {menu.map((item, i) =>{
+                                    return (
+                                        <a key={item.id} href={item.link}> {item.title} </a>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div className="">
+                        <p className=" text-2xl text-orange font-extrabold ">Trip Planner</p>
+                        <div>
+                        {tripPlanner.map((item, i) =>{
+                                    return (
+                                        <a key={item.id} href={item.link}> {item.title} </a>
+                                    );
+                                })}
+                
+                        </div>
+                    </div>
+                    <div>sdgrzsdg</div>
+                </div>
+                <hr></hr>
+            </div>
+            <div className=" bg-[#393535] h-24 ">
+
+            </div>
+            </div>
+            </div>
+            
+    );
+}
