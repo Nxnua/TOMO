d5fe260b7dc7a5968e2335a53fc40946cd2b094d
Gunjee007 <Gunjee163@gmail.com>
2023-10-19 14:26:16 +0800

    footer

--- resources/js/components/footer.jsx
+++ resources/js/components/footer.jsx
@@ -1,10 +1,31 @@
 export default function Footer(){
+    const menu = ["Trips", "Guides", "Agencies", "Reviews"];
+    const tripPlanner = ["Plan a trip", "Travel Budget", "Flights"];
     return(
-        <div>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+        
+        <div className=" bg-black75 flex flex-col ">
+            <div className=" h-72 ">
+                <div className=" h-full grid grid-cols-3">
+                    <div className=" relative flex items-center ">
+                        <img src="/images/tomo-T.png" alt="" className=" absolute  h-52 ml-36   " />
+                        <img src="/images/Tomo-White.png" alt="" className=" h-20 ml-44 " />
+                    </div>
+                    <div className=" bg-black flex flex-row p-12">
+                        <div>
+                            <p className=" text-2xl text-orange font-extrabold">Menu</p>
+                        </div>
+                        <div>
+                            <p>jsdg</p>
+                        </div>
+                    </div>
+                    <div>sdgrzsdg</div>
+                </div>
+                <hr></hr>
+            </div>
+            <div className=" bg-[#393535] h-24 ">
+
+            </div>
+            
         </div>
     );
 }
