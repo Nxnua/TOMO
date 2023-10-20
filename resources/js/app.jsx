c1357f1d3242a352c4ebde502b75895e6b53fb91
kisaragi1234 <beeerhembileg@gmail.com>
2023-10-20 13:02:21 +0800

    Guides

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,18 +2,10 @@ import './bootstrap';
 import '../css/app.css'
 
 import ReactDOM from 'react-dom/client';        
-<<<<<<< HEAD
-import Home from './Pages/Home';
+import Home from './Pages/Homepage/Home';
+import Header from './Pages/Homepage/header';
 import { Guides } from './Pages/Guides';
 
 ReactDOM.createRoot(document.getElementById('app')).render(     
     <Guides />        
 );
-=======
-import Home from './Pages/Homepage/Home';
-
-ReactDOM.createRoot(document.getElementById('app')).render(     
-    <Home />        
-);
-
->>>>>>> b3928751922ef4eda4068e62a7ee80ffb76ccc33
