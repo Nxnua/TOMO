d1b7cd837e7aba3a6d18730f9d9495e6eb99d284
Nxnua <nrnbtr760816@gmail.com>
2023-11-07 23:24:26 +0800

    vv

---
+++ resources/js/components/abus.jsx
@@ -0,0 +1,34 @@
+export default function Abus() { 
+	return ( 
+		<div > 
+			<div className="ml-156 text-center float-center mt-60 flex-col rounded-xlbg-white text-gray-700 shadow-2xl"> 
+				<div className="p-6"> 
+					<h5 className="text-center mr-4 mb-2 
+								block font-sans text-xl 
+								font-semibold text-blue-gray-900 
+								antialiased"> 
+						About us 
+					</h5> 
+					<ul> 
+						<li className="mt-2"> 
+							<span><i className="fa fa-phone mr-2"></i> </span> 
+							+976-99117788 
+						</li> 
+						<li className="mt-2"> 
+							<span><i className="fa fa-envelope mr-2"></i> </span> 
+							<span>feedbacktomo@gmail.com</span> 
+						</li> 
+						<li className="mt-2"> 
+							<span><i className="fa-solid fa-map-pin mr-2"></i> 
+							</span> 
+								Huree University, Bayngol district 
+							<span className="pl-4"> 
+								Ulaanbaatar, Mongolia
+							</span> 
+						</li> 
+					</ul> 
+				</div> 
+			</div> 
+		</div> 
+	) 
+}
