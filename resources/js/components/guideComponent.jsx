0fcb1acb3183bee23db185fcffba23d8a6617f69
kisaragi1234 <beeerhembileg@gmail.com>
2023-11-12 18:15:00 +0800

    detail

--- resources/js/components/guideComponent.jsx
+++ resources/js/components/guideComponent.jsx
@@ -4,32 +4,40 @@ export default function Guide({ name, description, image, review, star }) {
         <div className=" mt-4  bg-white rounded-xl h-[20vw] border-[1px] border-neutral-100 drop-shadow-[_0px_15px_15px_rgba(0,0,0,0.05)] flex flex-row">
             <div className=" w-[35%]  flex flex-col justify-between">
                 <div className=" rounded-lg border-[1px] border-neutral-200">
-                    <img src={image} alt="" className=" w-[180px] h-[150px] rounded-lg " />
+                    <img
+                        src={image}
+                        alt=""
+                        className=" w-[180px] h-[150px] rounded-lg "
+                    />
                 </div>
                 <div className="  flex justify-center ">{name}</div>
                 <div className=" grid grid-cols-3 gap-1 m-10 mb-20  ">
-                    <img src="images/flagmo.png " className=" h-5 w-5 " alt="" />
-                    <img src="images/flagko.png"  className=" h-5 w-5" alt="" />  
-                    <img src="images/flagen.png"  className=" h-5 w-5" alt="" /> 
-                                  
+                    <img
+                        src="images/flagmo.png "
+                        className=" h-5 w-5 "
+                        alt=""
+                    />
+                    <img src="images/flagko.png" className=" h-5 w-5" alt="" />
+                    <img src="images/flagen.png" className=" h-5 w-5" alt="" />
                 </div>
-                
-                
             </div>
-            
+
             <div className=" flex-1">
                 <div className=" flex h-16 justify-center items-center">
                     <div className="rating  justify-center flex flex-row  ml-48 mt-5 ">
-      
                         <div className="star  flex flex-col justify-center">
-                        <div>
-                    <Review reviews="122" rating="4.6" />
-                </div>
+                            <div>
+                                <Review reviews="122" rating="4.6" />
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="descrption h-[50%] flex flex-row ">
-                    <div className="m-4 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quod corporis eum amet distinctio est atque illo voluptatem aut accusantium.</div>
+                    <div className="m-4 text-justify">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Ad quod corporis eum amet distinctio est atque
+                        illo voluptatem aut accusantium.
+                    </div>
                 </div>
                 <div className="member flex flex-row ml-3 ">
                     <div>
@@ -37,28 +45,21 @@ export default function Guide({ name, description, image, review, star }) {
                     </div>
                     2-6
                     <div className="favorite flex ml-48">
-                        
                         <div className=" absolute flex justify-center ">
-                        <img
-                            src="/images/favorite.png"
-                            alt=""
-                            className=" hover:opacity-50 h-6 w-6"
-                        />
-                    </div>
-                        
+                            <img
+                                src="/images/favorite.png"
+                                alt=""
+                                className=" hover:opacity-50 h-6 w-6"
+                            />
+                        </div>
                     </div>
                 </div>
                 <div className=" transition ease-in-out delay-50  hover:scale-105  mt-2 drop-shadow-[_0px_4px_4px_rgba(0,0,0,0.2)] ">
-                <button className=" bg-white w-[75%] border-2 py-1 border-black50 rounded-lg font-extrabold tracking-tighter ">
-                    Book now
-                </button>
+                    <button className=" bg-white w-[75%] border-2 py-1 border-black50 rounded-lg font-extrabold tracking-tighter ">
+                        Book now
+                    </button>
+                </div>
             </div>
-            </div>
-            
-            
         </div>
-        
-        
-
     );
 }
