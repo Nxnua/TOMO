bc06b4a9980e88b26be94c66f402e6e8bcce65e9
Gunjee007 <gunjee163@gmail.com>
2023-11-23 14:38:20 +0800

    logo

--- resources/js/components/header.jsx
+++ resources/js/components/header.jsx
@@ -65,7 +65,7 @@ function Header() {
             <Link className="ms-10" to="/">
                 <img
                     src={
-                        darkMode ? "/images/Tomo-White.png" : "/images/tomo-new.png"
+                        darkMode ? "/images/tomo-new.png" : "/images/tomo-new.png"
                     }
                     alt=""
                     className="w-[13vw] h-[12vh]  mt-1 hover:opacity-80"
