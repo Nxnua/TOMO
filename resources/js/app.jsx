cc995114f7aebaec1890ce3bc3645965f63a4508
Nxnua <nrnbtr760816@gmail.com>
2023-11-23 14:50:15 +0800

    appjs soliw

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,10 @@
 import ReactDOM from "react-dom/client";
 import Home from "./Pages/Homepage/Home";
-import Plantrip from "./Pages/PlanTrip";
-import Trips from "./Pages/Trips";
-import { Guides } from "./Pages/Guides";
+import Plantrip from "./Pages/PlanTrip/PlanTrip";
+import {Days} from "./Pages/PlanTrip/days";
+import { Prefer } from "./Pages/PlanTrip/prefer";
+import Trips from "./Pages/Agency/Trips";
+import { Guides } from "./Pages/Guide/Guides";
 import Map from "./components/Map";
 import Movement from "./components/banner/bannerSVG";
 import Feedback from "./Pages/feedback";
@@ -11,16 +13,12 @@ import Faqclose from "./components/faq/faqClose";
 import Home1 from "./admin1/pages1/home1";
 //import App from "./admin1/App";
 import Single from "./admin1/pages1/Single";
-import { GuideDetail } from "./Pages/GuideDetail";
+import { GuideDetail } from "./Pages/Guide/GuideDetail";
 import Reviews from "./Pages/Reviews";
 import { TripDetail } from "./Pages/Agency/TripDetail";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SvgComponent from "./components/Map";
 import { Profile } from "./Pages/Profile";
-
-import  Notif  from "./components/Notif";
-
-
 import { ThemeContextProvider } from "./components/darkmode/states";
 
 
@@ -67,8 +65,8 @@ const router = createBrowserRouter([
         element: <Profile />,
     },
     {
-        path: "/Notif",
-        element: <Notif />,
+        path: "/feedback",
+        element: <Feedback />,
     },
     {
         path: "/admin",
@@ -82,7 +80,14 @@ const router = createBrowserRouter([
         path: "/reviews",
         element: <Reviews />,
     },
-
+    {
+        path: "/faq",
+        element: <Faq />,
+    },
+    {
+        path: "/faqq",
+        element: <Faqclose />,
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("app")).render(
