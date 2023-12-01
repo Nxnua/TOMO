de303f7825a99562be1b43383eac71d4d54ee4bb
Gunjee007 <gunjee163@gmail.com>
2023-12-02 00:03:11 +0800

    guide component group size

--- resources/js/components/home/guideComponent.jsx
+++ resources/js/components/home/guideComponent.jsx
@@ -3,7 +3,7 @@ import Review from "../review";
 export default function Guide({ guide }) {
     const { name, description, image, review, rate } = guide;
     return (
-        <div className="flex mb-5 dark:bg-darknav  bg-white dark:border-gray-800 rounded-2xl w-[450px] h-[300px] border border-neutral-100 drop-shadow-[_0px_15px_15px_rgba(0,0,0,0.05)]">
+        <div className="flex dark:bg-darknav  bg-white dark:border-gray-800 rounded-2xl w-[450px] h-[300px] border border-neutral-100 drop-shadow-[_0px_15px_15px_rgba(0,0,0,0.05)]">
             <div className=" w-[35%]  flex flex-col justify-between items-center">
                 <div className="float-left w-full h-[150px] rounded-2xl">
                     <img
@@ -42,21 +42,19 @@ export default function Guide({ guide }) {
                     </div>
                 </div>
                 <div className="descrption w-auto h-[50%] flex flex-row ">
-                    <div className="w-auto m-4 text-justify">{description}</div>
+                    <div className="w-auto m-4 text-sm font-medium text-justify">{description}</div>
                 </div>
-                <div className="w-full flex flex-row justify-start ml-3 space-x-[75%]">
-                    <div>
+                <div className=" flex flex-row justify-between mx-3 ">
+                    <div className=" flex flex-row items-center"> 
                         <img src="/images/hun.png" alt="" />
-                        2-6
+                       <p> 2-6</p>
                     </div>
-                    <div className="favorite flex">
-                        <div className=" absolute flex justify-center ">
+                    <div className="favorite ">
                             <img
                                 src="/images/favorite.png"
                                 alt=""
                                 className=" hover:opacity-50 h-6 w-6"
                             />
-                        </div>
                     </div>
                 </div>
                 <div className=" transition ease-in-out delay-50  hover:scale-105  mt-5 drop-shadow-[_0px_4px_4px_rgba(0,0,0,0.2)] ">
